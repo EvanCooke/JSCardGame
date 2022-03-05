19d7728aee51b7cbccd8b565d850b098554a1bb1
Evan Cooke <evncooke@gmail.com>
2022-03-05 02:28:48 -0600

    added css and html elements

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// To run it as a module, it needs to run on a web server
+// https://stackoverflow.com/questions/52139811/javascript-module-not-working-in-browser
 import Deck from './deck.js'
 
 const deck = new Deck()
+deck.shuffle()
 console.log(deck.cards)
